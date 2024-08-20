3a8a7e2e42875efa520930ce1c08f9cf3e8b17a4
ThatGuySam <sam@sam.lc>
2024-08-20 12:27:11 -0500

    Add ColorLockup to color screen

--- app/color/[color].tsx
+++ app/color/[color].tsx
@@ -1,7 +1,30 @@
+import { ColorLockup, colors } from "@/components/ColorLockup";
+import { useLocalSearchParams } from "expo-router";
 import { Text } from "react-native";
 
+function useColor () {
+    const { color } = useLocalSearchParams();
+    
+    if ( !color || Array.isArray(color) ) {
+        throw new Error("Color is required")
+    }
+
+    for (const key in colors) {
+      if (colors[key].hex === color) {
+        return colors[key]
+      }
+    }
+
+    throw new Error("Invalid color")
+}
+
 export default function ColorScreen() {
+    const color = useColor()
+    
     return (
-      <Text>Color!</Text>
+      <>
+        <Text>Color!{ color.hex }</Text>
+        <ColorLockup item={ color } />
+      </>
     );
-  }
+}
